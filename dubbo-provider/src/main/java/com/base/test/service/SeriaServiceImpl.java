52e483e182dfc1a36d0d025de18fcb207b53909c
wangxuan2 <wangxuan2@jk.cn>
2016-08-29 17:05:30 +0800

    新型高效序列化实现  待续

---
+++ dubbo-provider/src/main/java/com/base/test/service/SeriaServiceImpl.java
@@ -0,0 +1,13 @@
+package com.base.test.service;
+
+/**
+ * 新型序列化：支持基于Kryo和FST的Java高效序列化实现,参考dubbox
+ * http://www.open-open.com/lib/view/open1417426480618.html
+ * @decription:
+ * @author wangxuan2
+ * @date 2016年8月29日 下午5:04:02
+ */
+
+public class SeriaServiceImpl {
+
+}
